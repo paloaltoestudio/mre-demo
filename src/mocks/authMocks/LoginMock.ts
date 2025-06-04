ebd077fcb0b1ac15693d7d53a96546e8c1c1bc04
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2025-06-04 10:54:09 -0500

    fix: types of documents

--- src/mocks/authMocks/LoginMock.ts
+++ src/mocks/authMocks/LoginMock.ts
@@ -1,10 +1,10 @@
 export const documentTypes = [
   { value: "CC", label: "Cédula de Ciudadanía" },
-  { value: "TI", label: "Tarjeta de Identidad" },
+  // { value: "TI", label: "Tarjeta de Identidad" },
   { value: "CE", label: "Cédula de Extranjería" },
-  { value: "PA", label: "Pasaporte" },
-  { value: "NIT", label: "NIT (Número de Identificación Tributaria)" },
-  { value: "PEP", label: "Permiso Especial de Permanencia" },
-  { value: "RC", label: "Registro Civil" },
-  { value: "NUIP", label: "Número Único de Identificación Personal" },
+  // { value: "PA", label: "Pasaporte" },
+  // { value: "NIT", label: "NIT (Número de Identificación Tributaria)" },
+  // { value: "PEP", label: "Permiso Especial de Permanencia" },
+  // { value: "RC", label: "Registro Civil" },
+  // { value: "NUIP", label: "Número Único de Identificación Personal" },
 ];
