39d3a9178f81be310fe542ea374384820ce48461
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2025-07-29 08:16:33 -0500

    Fix: The scheduling and cancellation of appointments were adjusted

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "./app.css";
 import { MainRouter } from "./Routers/MainRouter.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 
 const queryClient = new QueryClient();
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <MainRouter />
       {/* Comentar el devtools para produccion */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>
 );
