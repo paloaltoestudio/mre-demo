02d7e12f412e84d20aea99a790ec680dd62b655d
David <usmagrafico@gmail.com>
2026-05-20 16:47:46 -0500

    Update import statement for App.css to follow consistent naming conventions in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import { MainRouter } from "./Routers/MainRouter.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
